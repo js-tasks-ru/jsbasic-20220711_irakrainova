bfc6438b6c732807ef424781062f3b3763bae174
irakrainova <irakrainova@yandex.ru>
2022-07-27 12:55:42 +0300

    задания 1 урока

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,13 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str.length <= maxlength) {
+    return (str);
+  }
+  else {
+    const k = (str.length - maxlength);
+    let str1 = str.slice(0,maxlength-1);
+    const d = str1 + "…";
+    return (d);
+  }
 }
+let a = 'Вот, что мне хотелось бы сказать на эту тему:';
+console.log (truncate( a, 10))
